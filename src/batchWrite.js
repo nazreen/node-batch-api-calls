5855c36d99dd7a2b66110e78add13907c4a319bc
Nazreen Mohamad <nxmohamad@gmail.com>
2020-04-17 23:19:37 +0100

    create skeleton

--- src/batchWrite.js
+++ src/batchWrite.js
@@ -5,6 +5,24 @@ const { INPUT_FILE, OUTPUT_FILE } = config
 
 async function batchWrite() {
   const documents = await readFile(INPUT_FILE)
+  documents.forEach(processDocument)
+  commit()
 }
+
+function processDocument(document) {
+  // TODO: transform
+  // TODO: check against schema
+  addToTransaction(document)
+}
+
+function addToTransaction(document) {
+  // TODO: add to transaction
+}
+
+function commit() {
+  //  TODO: commit the batch transaction
+}
+
+
 
 batchWrite()
