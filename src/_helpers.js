ccd76166862619d5520c450c26f290b8503fe501
Nazreen Mohamad <nxmohamad@gmail.com>
2020-04-17 23:12:10 +0100

    create readFile helper

--- src/_helpers.js
+++ src/_helpers.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const fs = require('fs')
+const neatCsv = require('neat-csv');
 
 function appendJsonFileName(fileName, textToAppend) {
   const array = fileName.split(/([.])/g)
@@ -11,5 +13,17 @@ function getFileExtension(input) {
   return path.extname(input)
 }
 
+async function readFile(inputPath) {
+  const fileExtension = getFileExtension(inputPath)
+  let raw = fs.readFileSync(inputPath)
+  switch (fileExtension) {
+    case '.csv':
+      return await neatCsv(raw)
+    default:
+      return raw
+  }
+}
+
 exports.appendJsonFileName = appendJsonFileName
 exports.getFileExtension = getFileExtension
+exports.readFile = readFile
